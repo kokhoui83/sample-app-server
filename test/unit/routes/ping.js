a6c48fd1f7ece1aabe45adffb46cc5fa6b19d54c
brian lim <kokhoui.lim@duerrdental.com>
2022-10-05 14:08:54 +0800

    rename test structure

--- test/unit/routes/ping.js
+++ test/unit/routes/ping.js
@@ -1,8 +1,8 @@
 const assert = require('assert')
 const ping = require('../../../src/routes/ping')
 
-describe('GET /ping', () => {
-    describe('Trigger /ping handler', () => {
+describe('Route /ping', () => {
+    context('Trigger /ping handler', () => {
         it('should return pong', () => {
             const result = ping.ping.handler(null, null)
             assert.equal('pong', result)
